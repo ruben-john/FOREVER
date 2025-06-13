8070038f6f7b5f1ad3236795a9facbb5ee6cc110
Ruben John <rubensajeev@gmail.com>
2025-06-13 19:10:37 +0530

    changed home to Home

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Routes,Route } from 'react-router-dom'
-import Home from './pages/home'
+import Home from './pages/Home'
 import About from './pages/About'
 import Collection from './pages/Collection'
 import Contact from './pages/Contact'
